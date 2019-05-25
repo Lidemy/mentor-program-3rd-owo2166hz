26965decd34686891e37232d5f8ce3d0e2e09b4c
owo2166hz <49502834+owo2166hz@users.noreply.github.com>
2019-05-25 21:21:55 +0800

    Update hw5.js

--- homeworks/week3/hw5.js
+++ homeworks/week3/hw5.js
@@ -1,5 +1,15 @@
 function add(a, b) {
-  console.log(a, b);
+  let toAdd = 0;
+  let answer = '';
+  for (let i = 1, j = 1; a.length - i >= 0 || b.length - j >= 0 || toAdd; i ++ , j ++) {
+    const tmp = toAdd + parseInt(a[a.length - i] || 0, 10) + parseInt(b[b.length - i] || 0, 10);
+    if (tmp < 10) {
+      answer = tmp + answer;
+      toAdd = 0;
+    } else {
+      answer = (tmp - 10) + answer;
+      toAdd = 1;
+    }
+  }
+  return answer;
 }
-
-module.exports = add;
