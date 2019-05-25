2801820f3333da8db526947007eca36df8dbd641
owo2166hz <49502834+owo2166hz@users.noreply.github.com>
2019-05-25 21:19:51 +0800

    Update hw2.test.js

--- homeworks/week3/hw2.test.js
+++ homeworks/week3/hw2.test.js
@@ -1,7 +1,10 @@
 const alphaSwap = require('./hw2');
 
-describe('hw2', () => {
+describe('hw2 大小寫互換', () => {
   it('should return correct answer when str = nick', () => {
     expect(alphaSwap('nick')).toBe('NICK');
+  });
+    it('should return correct answer when str = nick', () => {
+    expect(alphaSwap('owo123')).toBe('OWO123');
   });
 });
