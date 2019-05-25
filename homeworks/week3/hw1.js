50131cfca217a56de5ffea1b09ba3f03b7d73c00
owo2166hz <49502834+owo2166hz@users.noreply.github.com>
2019-05-25 21:16:15 +0800

    Update hw1.js

--- homeworks/week3/hw1.js
+++ homeworks/week3/hw1.js
@@ -1,5 +1,10 @@
 function stars(n) {
-  console.log(n);
+  const result = [];
+  for (let i = 1; i <= n; i += 1) {
+    result.push('*'.repeat(i));
+  }
+  return result;
 }
-
+//stars(1,3,6);
+//console.log(n);
 module.exports = stars;
