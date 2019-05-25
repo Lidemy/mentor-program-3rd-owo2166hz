e2a51f739a7b8d202a0e6a516d8912a0ff0268ce
owo2166hz <49502834+owo2166hz@users.noreply.github.com>
2019-05-25 21:21:32 +0800

    Update hw4.test.js

--- homeworks/week3/hw4.test.js
+++ homeworks/week3/hw4.test.js
@@ -1,7 +1,16 @@
 const isPalindromes = require('./hw4');
 
-describe('hw4', () => {
-  it('should return correct answer when str = abcdcba', () => {
-    expect(isPalindromes('abcdcba')).toBe(true);
+describe('hw4 判斷迴文', () => {
+  it('should return correct answer when str = owo', () => {
+    expect(isPalindromes('owo')).toBe(true);
+  });
+    it('should return correct answer when str = zxcvcxz', () => {
+    expect(isPalindromes('zxcvcxz')).toBe(true);
+  });
+      it('should return correct answer when str = dwsfergthy', () => {
+    expect(isPalindromes('dwsfergthy')).toBe(false);
+  });
+        it('should return correct answer when str = aaaaaaa', () => {
+    expect(isPalindromes('aaaaaaa')).toBe(true);
   });
 });
