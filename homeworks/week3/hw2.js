29c022656bb9d6379473ffec33c07711cc4161e0
owo2166hz <49502834+owo2166hz@users.noreply.github.com>
2019-05-25 21:17:39 +0800

    Update hw2.js

--- homeworks/week3/hw2.js
+++ homeworks/week3/hw2.js
@@ -1,5 +1,11 @@
 function alphaSwap(str) {
-  console.log(str);
+ let resylt = '';
+ for (let i = 0; i < str.length; i += 1) {
+   	resylt +=str[i] >= 'a' && str[i] <='z' ? str[i].toUpperCase() : str[i].toLowerCase();
+   }  
+   return resylt
 }
+//alphaSwap("ABCD");
+//alphaSwap("a1lKK2");
 
 module.exports = alphaSwap;
