5a76a471c2e3d3f4198061ef1ca315244c735055
owo2166hz <49502834+owo2166hz@users.noreply.github.com>
2019-05-25 21:20:22 +0800

    Update hw3.test.js

--- homeworks/week3/hw3.test.js
+++ homeworks/week3/hw3.test.js
@@ -1,7 +1,25 @@
 const isPrime = require('./hw3');
 
-describe('hw3', () => {
+describe('hw3 判斷質數', () => {
   it('should return correct answer when n = 1', () => {
     expect(isPrime(1)).toBe(false);
+  });
+  it('should return correct answer when n = 2', () => {
+    expect(isPrime(2)).toBe(true);
+  });
+  it('should return correct answer when n = 4', () => {
+    expect(isPrime(4)).toBe(false);
+  });
+  it('should return correct answer when n = 7', () => {
+    expect(isPrime(7)).toBe(true);
+  });
+  it('should return correct answer when n = 13', () => {
+    expect(isPrime(13)).toBe(true);
+  });
+  it('should return correct answer when n = 19', () => {
+    expect(isPrime(19)).toBe(true);
+  });
+  it('should return correct answer when n = 30', () => {
+    expect(isPrime(30)).toBe(false);
   });
 });
