a300e100236afcf32489a0b459bc543ce5ccaab8
owo2166hz <49502834+owo2166hz@users.noreply.github.com>
2019-05-25 21:16:56 +0800

    Update hw1.test.js

--- homeworks/week3/hw1.test.js
+++ homeworks/week3/hw1.test.js
@@ -1,6 +1,6 @@
 const stars = require('./hw1');
 
-describe('hw1', () => {
+describe('hw1 好多星星', () => {
   it('should return correct answer when n = 1', () => {
     expect(stars(1)).toEqual(['*']);
   });
