1df7e663bbce363c519a4839928cd89aed2688f6
owo2166hz <49502834+owo2166hz@users.noreply.github.com>
2019-05-25 21:20:05 +0800

    Update hw3.js

--- homeworks/week3/hw3.js
+++ homeworks/week3/hw3.js
@@ -1,5 +1,13 @@
 function isPrime(n) {
-  console.log(n);
+  let count = 0;
+  for (let i = 1; i < n; i += 1) {
+    if (n % i === 0) {
+      count += 1;
+    }
+  }
+  return count === 1;
 }
-
-module.exports = isPrime;
+isPrime(2);
+isPrime(40);
+//console.log(isPrime(7));
+//module.exports = isPrime;
